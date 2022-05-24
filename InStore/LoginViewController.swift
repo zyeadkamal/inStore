a1d0d0448d295cf592ac4a53a904a4f41669c4b7
zyeadkamal <66483556+zyeadkamal@users.noreply.github.com>
2022-05-24 11:19:16 +0200

    init project

---
+++ InStore/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InStore
+//
+//  Created by mac on 5/24/22.
+//  Copyright © 2022 mac. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
