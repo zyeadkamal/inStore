b7badb408db70ac1c3c44a693094e3157c488e00
SandraGeorge19 <50073317+SandraGeorge19@users.noreply.github.com>
2022-05-30 05:12:07 -0700

    feat: finishing Wishlist Screen UI

--- InStore/View/WishlistScreen/WishlistScreenView/WishlistViewController.swift
+++ InStore/View/WishlistScreen/WishlistScreenView/WishlistViewController.swift
@@ -10,12 +10,49 @@ import UIKit
 
 class WishlistViewController: UIViewController {
 
+    //MARK: -- IBOutlets
+    @IBOutlet weak var wishlistTableView: UITableView!
+    @IBOutlet weak var suggestedCollectionView: UICollectionView!
+    @IBOutlet weak var noWishlistImg: UIImageView!
+    
+    
+    
+    //MARK: -- Properties
+    var wishlistItems : [String] = []
+    
+    
+    //MARK: -- Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        registerWishlistCell()
+        configureWishlistTableView()
+        registerSuggestedCollectionView()
+        configureSuggestedCollectionView()
     }
     
+    //MARK: -- IBActions
+    
+    
+    //MARK: -- Functions
+    func configureWishlistTableView() {
+        wishlistTableView.delegate = self
+        wishlistTableView.dataSource = self
+    }
+    
+    func registerWishlistCell(){
+        self.wishlistTableView.register(UINib(nibName: "WishlistTableViewCell", bundle: nil), forCellReuseIdentifier: "cell")
+    }
+    
+    func configureSuggestedCollectionView(){
+        suggestedCollectionView.delegate = self
+        suggestedCollectionView.dataSource = self
+    }
+    
+    func registerSuggestedCollectionView(){
+        self.suggestedCollectionView.register(UINib(nibName: "SuggestedCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "cell")
+    }
 
     /*
     // MARK: - Navigation
@@ -27,4 +64,49 @@ class WishlistViewController: UIViewController {
     }
     */
 
+}
+
+extension WishlistViewController : UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if (wishlistItems.isEmpty){
+            noWishlistImg.isHidden = false
+            wishlistTableView.isHidden = true
+        }else{
+            noWishlistImg.isHidden = true
+            wishlistTableView.isHidden = false
+        }
+        return wishlistItems.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let wishlistCell = wishlistTableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! WishlistTableViewCell
+        wishlistCell.wishlistProductTitle.text = "Helloooo"
+        wishlistCell.wishlistProductPrice.text = "$53.48"
+        return wishlistCell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 120
+    }
+}
+
+
+extension WishlistViewController : UICollectionViewDelegate, UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let suggestedCell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! SuggestedCollectionViewCell
+        suggestedCell.suggestedProductTitle.text = "Kids Shoes"
+        suggestedCell.suggestedProductPrice.text = "$65.2"
+        suggestedCell.suggestedProductDesc.text = "Amet minisit aliqua dolor do amet sint."
+        suggestedCell.productRatting.text = "5.0 (35)"
+        return suggestedCell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize{
+        return CGSize(width: 250, height:  200)
+    }
 }
